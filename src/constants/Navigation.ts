4b02e84fd3e3917c6110d7735c13005a0c8fbc98
Hwuiwon Kim <hwuiwon.kim@gmail.com>
2023-08-16 12:38:51 -0400

    added textarea for search

---
+++ src/constants/Navigation.ts
@@ -0,0 +1,41 @@
+import {
+  Bars3BottomLeftIcon,
+  BuildingOffice2Icon,
+  MagnifyingGlassIcon,
+} from '@heroicons/react/24/outline';
+
+export interface NavigationElementProps {
+  name: string;
+  href: string;
+  icon: any;
+  current: boolean;
+}
+
+export enum Page {
+  SEARCH,
+  HISTORY,
+  ADMIN,
+}
+
+export const getNavigationItems = (
+  selected: Page
+): NavigationElementProps[] => [
+  {
+    name: 'Search',
+    href: '/search',
+    icon: MagnifyingGlassIcon,
+    current: selected === Page.SEARCH,
+  },
+  {
+    name: 'My History',
+    href: '#',
+    icon: Bars3BottomLeftIcon,
+    current: selected === Page.HISTORY,
+  },
+  {
+    name: 'Admin Dashboard',
+    href: '/admin',
+    icon: BuildingOffice2Icon,
+    current: selected === Page.ADMIN,
+  },
+];
