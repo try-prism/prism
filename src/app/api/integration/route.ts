61a03470326925181a3f83c7b0edf70748dfa91b
Hwuiwon Kim <hwuiwon.kim@gmail.com>
2023-08-16 23:27:04 -0400

    updated delete api

--- src/app/api/integration/route.ts
+++ src/app/api/integration/route.ts
@@ -41,4 +41,35 @@ export async function POST(req: Request) {
   }
 }
 
-export async function DELETE(req: Request) {}
+export async function DELETE(req: Request) {
+  const { token, organizationId, accountToken } = await req.json();
+
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}/integration/${organizationId}`,
+      {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'integration-account-token': accountToken,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      console.log('Error from API side: ' + response.statusText);
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error: unknown) {
+    console.error('Error retrieving API response: ' + error);
+
+    return NextResponse.json(
+      new APIException(
+        APIExceptionCode.UNKNOWN_ERROR,
+        error instanceof Error ? error.message : 'Unknown Error'
+      ).toJSON()
+    );
+  }
+}
