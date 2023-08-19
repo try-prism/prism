e21e61e6f6d21a93b196079b51134afcd7b0bbdc
Hwuiwon Kim <hwuiwon.kim@gmail.com>
2023-08-19 15:48:49 -0400

    color change

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -130,7 +130,7 @@ export default function UserList({ organization }: UserListsProps) {
             <div className="ml-4 mt-2 flex-shrink-0">
               <button
                 type="button"
-                className="relative inline-flex items-center rounded-md bg-purple-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
+                className="relative inline-flex items-center rounded-md bg-purple-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
                 onClick={() => setShowUserInviteModal(true)}
               >
                 Invite
