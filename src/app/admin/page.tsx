7c8a56f479451f2dd4ea8870e57b7c463e0fbbc5
Hwuiwon Kim <hwuiwon.kim@gmail.com>
2023-08-23 09:16:40 -0400

    more ui changes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,16 +3,20 @@
 import { useEffect, useState } from 'react';
 
 import AdminInfo from '@/components/Admin/AdminInfo';
+import AdminSettingsTab from '@/components/Admin/AdminSettingsTab';
 import IntegrationCards from '@/components/Admin/IntegrationCards';
-import SettingsTab from '@/components/Admin/SettingsTab';
 import Sidebar from '@/components/Sidebar';
 import UserList from '@/components/UserList';
 import { TEST_ORG_ID, TEST_TOKEN } from '@/constant';
+import { AdminSettings } from '@/constants/AdminSettings';
 import { Page } from '@/constants/Navigation';
 import { Convert, Organization } from '@/models/Organization';
 
 export default function AdminPage() {
   const [organization, setOrganization] = useState<Organization>();
+  const [currentTab, setCurrentTab] = useState<AdminSettings>(
+    AdminSettings.OVERVIEW
+  );
   const organizationId = TEST_ORG_ID;
   const token = TEST_TOKEN;
 
@@ -43,10 +47,19 @@ export default function AdminPage() {
       {organization && (
         <main className="xl:pl-72 py-2">
           <div className="pl-4 pr-2 flex flex-col gap-y-3 mt-5 mx-5">
-            <SettingsTab />
-            <AdminInfo organization={organization} />
-            <IntegrationCards organization={organization} />
-            <UserList organization={organization} />
+            <AdminSettingsTab
+              currentTab={currentTab}
+              setCurrentTab={setCurrentTab}
+            />
+            {currentTab === AdminSettings.OVERVIEW && (
+              <AdminInfo organization={organization} />
+            )}
+            {currentTab === AdminSettings.INTEGRATIONS && (
+              <IntegrationCards organization={organization} />
+            )}
+            {currentTab === AdminSettings.USERS && (
+              <UserList organization={organization} />
+            )}
           </div>
         </main>
       )}
--- src/components/Admin/AdminSettingsTab.tsx
+++ src/components/Admin/AdminSettingsTab.tsx
@@ -1,14 +1,24 @@
 import clsx from 'clsx';
-import Link from 'next/link';
+import { useMemo } from 'react';
 
-const tabs = [
-  { name: 'Organization Overview', href: '#', current: true },
-  { name: 'Users', href: '#', current: false },
-  { name: 'Billing', href: '#', current: false },
-  { name: 'Integraions', href: '#', current: false },
-];
+import {
+  AdminSettings,
+  getAdminNavigationItems,
+} from '@/constants/AdminSettings';
 
-export default function SettingsTab() {
+interface AdminSettingsTabProps {
+  currentTab: AdminSettings;
+  setCurrentTab: (tab: AdminSettings) => void;
+}
+
+export default function AdminSettingsTab({
+  currentTab,
+  setCurrentTab,
+}: AdminSettingsTabProps) {
+  const tabs = useMemo(() => {
+    return getAdminNavigationItems(currentTab);
+  }, [currentTab]);
+
   return (
     <div className="border-b border-gray-200">
       <h3 className="text-base font-semibold leading-6 text-gray-900">
@@ -24,6 +34,7 @@ export default function SettingsTab() {
             name="current-tab"
             className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-purple-500 focus:outline-none focus:ring-purple-500 sm:text-sm"
             defaultValue={tabs.find(tab => tab.current)?.name}
+            onChange={e => setCurrentTab(e.target.value as AdminSettings)}
           >
             {tabs.map(tab => (
               <option key={tab.name}>{tab.name}</option>
@@ -33,19 +44,19 @@ export default function SettingsTab() {
         <div className="hidden sm:block">
           <nav className="-mb-px flex space-x-8">
             {tabs.map(tab => (
-              <Link
+              <button
                 key={tab.name}
-                href={tab.href}
                 className={clsx(
                   tab.current
                     ? 'border-purple-500 text-purple-600'
                     : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
                   'whitespace-nowrap border-b-2 px-1 pb-4 text-sm font-medium'
                 )}
+                onClick={() => setCurrentTab(tab.name as AdminSettings)}
                 aria-current={tab.current ? 'page' : undefined}
               >
                 {tab.name}
-              </Link>
+              </button>
             ))}
           </nav>
         </div>
